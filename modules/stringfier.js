f600343aa95c597c165f5c66f925a6b337b4fc05
aster-alemu <leaster2014w@gmail.com>
2022-11-01 05:55:36 -0700

    Add all files

---
+++ modules/stringfier.js
@@ -0,0 +1,7 @@
+import { Store } from './store.js';
+
+const Stringifier = (() => {
+  const updatedBooks = JSON.stringify(Store.books);
+  localStorage.setItem('book', updatedBooks);
+});
+export { Stringifier };
